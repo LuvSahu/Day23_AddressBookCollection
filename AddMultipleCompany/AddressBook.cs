25e54f710f2bc5f097ddc541005f4f6ac2ad2ec5
Luv sahu <lksahu404@gmail.com>
2022-07-05 18:50:30 +0530

    [Luv SAHU] The code for Add Multiple Company

---
+++ AddMultipleCompany/AddressBook.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookCollectionsDay23
+{
+    internal class AddressBook
+    {
+        Contact tempContact = new Contact();
+        public Dictionary<string, Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new Dictionary<string, Contact>();
+        }
+
+        public void CreateContact()
+        {
+            Contact tempContact = new Contact();
+            tempContact.UserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+        //Method Used To Add Contacts
+        public void AddContacts()
+        {
+            tempContact.UserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+                Console.WriteLine("Successfully Added A New Contact!!!");
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+
+        }
+
+        //Method Used To Edit Contacts
+        public void EditContact()
+        {
+            Console.WriteLine("Enter name of contact to edit: ");
+            string name = Console.ReadLine();
+            if (contacts.ContainsKey(name) is true)
+            {
+                Contact tempContact = new Contact();
+                tempContact.UserInfo();
+                string editName = tempContact.GetName();
+                if (contacts.ContainsKey(editName) is false || editName == name)
+                {
+                    contacts.Remove(name);
+                    contacts.Add(editName, tempContact);
+                    Console.WriteLine("Successfully Edited And Saved!!!");
+                    Display();
+                }
+                else
+                    Console.WriteLine("Edited name is invalid");
+            }
+            else
+                Console.WriteLine("Name does not exist");
+        }
+
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter name of contact to delete: ");
+            string name = Console.ReadLine();
+            if (contacts.ContainsKey(name) is true)
+            {
+                contacts.Remove(name);
+                Console.WriteLine("Successfully Deleted!!!");
+            }
+            else
+                Console.WriteLine("Name does not exist");
+        }
+
+        public void AddMultiple()
+        {
+            Console.WriteLine("Enter no of contacts to add");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                CreateContact();
+            }
+            Display();
+            Console.WriteLine("Successfully Added New Contacts");
+        }
+
+
+        public void Display()
+        {
+            foreach (string name in contacts.Keys)
+                contacts[name].Display();
+        }
+    }
+}
---
+++ AddMultipleCompany/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AddressBookCollectionsDay23
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string Commit = " ";
+            Console.WriteLine("Welcome To Address Book ");
+
+
+            while (Commit != "Exit")
+            {
+
+                Console.WriteLine("1 for Add Contacts");
+                Console.WriteLine("2 for Display ");
+                Console.WriteLine("3 for Edit Contacts");
+                Console.WriteLine("4 for Delete for Contacts");
+                Console.WriteLine("5 for Adding Multiple Contacts");
+                int Opition = Convert.ToInt32(Console.ReadLine());
+
+                switch (Opition)
+                {
+                    case 1:
+                        AddressBook Address = new AddressBook();
+                        Address.CreateContact();
+                        break;
+                    case 2:
+                        AddressBook Disply = new AddressBook();
+                        Disply.Display();
+                        break;
+                    case 3:
+                        AddressBook Edit = new AddressBook();
+                        Edit.EditContact();
+                        break;
+                    case 4:
+                        AddressBook Delete = new AddressBook();
+                        Delete.DeleteContact();
+                        break;
+                    case 5:
+
+                        AddressBook AddMul = new AddressBook();
+                        AddMul.AddMultiple();
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Enter Vaild Input");
+                        Commit = "Exit";
+                        break;
+                }
+            }
+        }
+    }
+ }
