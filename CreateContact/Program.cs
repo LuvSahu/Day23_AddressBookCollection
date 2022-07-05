e81368f2f5b126506c2cdddd5ef275ab978c66a1
Luv sahu <lksahu404@gmail.com>
2022-07-05 17:50:53 +0530

    [Luv SAHU] Creating the contact

---
+++ CreateContact/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBookCollectionsDay23
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+        }
+    }
+}
