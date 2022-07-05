fe12f77826f584a4bd3621da3bbead28662e1704
Luv sahu <lksahu404@gmail.com>
2022-07-05 18:01:22 +0530

    [Luv SAHU] The code for Add contact

---
+++ AddContact/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AddressBookCollectionsDay23
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("===================Welcome To Address Book===================");
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+            Console.ReadKey();
+        }
+    }
+}
