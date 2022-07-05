46925f3c66b4a89ba875782f14281cd4b9e1ea98
Luv sahu <lksahu404@gmail.com>
2022-07-05 18:42:32 +0530

    [Luv SAHU] The code for Delete contact

---
+++ DeleteContact/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AddressBookCollectionsDay23
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string Commit = " ";
+            Console.WriteLine("Welcome To Address Book ");
+
+
+            while (Commit != "Exit")
+            {
+
+                Console.WriteLine("1 for Add Contacts");
+                Console.WriteLine("2 for Display ");
+                Console.WriteLine("3 for Edit Contacts");
+                Console.WriteLine("4 for Delete for Contacts");
+
+                int Opition = Convert.ToInt32(Console.ReadLine());
+
+                switch (Opition)
+                {
+                    case 1:
+                        AddressBook Address = new AddressBook();
+                        Address.CreateContact();
+                        break;
+                    case 2:
+                        AddressBook Disply = new AddressBook();
+                        Disply.Display();
+                        break;
+                    case 3:
+                        AddressBook Edit = new AddressBook();
+                        Edit.EditContact();
+                        break;
+                    case 4:
+                        AddressBook Delete = new AddressBook();
+                        Delete.DeleteContact();
+                        break;
+                    default:
+                        Console.WriteLine("Enter Vaild Input");
+                        Commit = "Exit";
+                        break;
+                }
+            }
+        }
+    }
+ }
