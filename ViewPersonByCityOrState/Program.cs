f27bfd930557e4a78e49f3d6b4246f961e66fc29
Luv sahu <lksahu404@gmail.com>
2022-07-06 10:12:48 +0530

    [Luv SAHU] Search the person with the city or state

---
+++ ViewPersonByCityOrState/Program.cs
@@ -0,0 +1,16 @@
+﻿using static AddressBookCollectionsDay23.AddressBook;
+
+namespace AddressBookCollectionsDay23
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program");
+            AddressBookLibrary bookLibrary = new AddressBookLibrary();
+            bookLibrary.AddressBookMenu();
+            Console.ReadLine();
+        }
+    }
+    
+ }
