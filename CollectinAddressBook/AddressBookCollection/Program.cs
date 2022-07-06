ea4045b619fa8b32c94e46c1f782f4e1c44b81b2
Luv sahu <lksahu404@gmail.com>
2022-07-06 11:59:12 +0530

    [Luv SAHU] Refactored Complete Code and Implemented Count Person by state or city

---
+++ CollectinAddressBook/AddressBookCollection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookCollection
+{
+
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Welcome To Address Book Program");
+        //    AddressBookMain bookLibrary = new AddressBookMain();
+        //    AddressBookMain.Menu();//going inside this class and method
+        //    MainMenu(string bookname)
+
+        //}
+    }
+}
+
+//to execute this
+//first we have to select the Option from Multiple addressBook in that 1 i
+//aftre that agin we have to open that addressbook to add the contact details
