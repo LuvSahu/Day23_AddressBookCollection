83bd27b0b6d2f3c3f649ff04884d43e2187e6b3b
Luv sahu <lksahu404@gmail.com>
2022-07-06 10:44:35 +0530

    [Luv SAHU] Read and Write to from CSV file

---
+++ CollectionAddressBook/AddressBookCollection/FileWriter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookCollection
+{
+    internal class FileWriter
+    {
+        public static string path = @"D:\Bridgelabz\AddressBookCollection\AddressBookFile.txt";
+        public static string csvPath = @"D:\Bridgelabz\AddressBookCollection\AddressBook.csv";
+        public static void WriteUsingStreamWriter(List<ContactPerson> data)
+        {
+
+            if (File.Exists(path))
+            {
+                File.WriteAllText(path, String.Empty);
+                using (StreamWriter streamWriter = File.AppendText(path))
+                {
+                    streamWriter.WriteLine("FirstName\tLastName\t Address\t City\t State\t Zip\t Contact\t Email");
+                    foreach (ContactPerson contacts in data)
+                    {
+                        streamWriter.WriteLine(contacts.firstName + "\t" + contacts.lastName + "\t" + contacts.address + "\t" + contacts.city + "\t" + contacts.state + "\t" + contacts.zip + "\t" + contacts.contact + "\t" + contacts.email);
+                    }
+                    streamWriter.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not avilable..");
+            }
+        }
+
+        public static void readFile()
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    string data = "";
+                    while ((data = streamReader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(data);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not avilable..");
+            }
+        }
+        public static void csvFileWriter(List<ContactPerson> dataa)
+        {
+
+            if (File.Exists(csvPath))
+            {
+                File.WriteAllText(csvPath, String.Empty);
+                using (StreamWriter streamWriter = File.AppendText(csvPath))
+                {
+                    streamWriter.WriteLine("FirstName,LastName,Address,City,State,Zip,Contact,Email");
+                    foreach (ContactPerson contacts in dataa)
+                    {
+                        streamWriter.WriteLine(contacts.firstName + "," + contacts.lastName + "," + contacts.address + "," + contacts.city + "," + contacts.state + "," + contacts.zip + "," + contacts.contact + "," + contacts.email);
+                    }
+                    streamWriter.Close();
+                    Console.WriteLine("Contacts Stored in Csv_File.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not avilable..");
+            }
+        }
+        public static void readFromCSVFile()
+        {
+            if (File.Exists(csvPath))
+            {
+                using (StreamReader streamReader = File.OpenText(csvPath))
+                {
+                    string data = "";
+                    while ((data = streamReader.ReadLine()) != null)
+                    {
+                        string[] csv = data.Split(",");
+                        foreach (string dataCsv in csv)
+                        {
+                            Console.Write(dataCsv + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not avilable..");
+            }
+        }
+    }
+}
---
+++ CollectionAddressBook/AddressBookCollection/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookCollection
+{
+
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Welcome To Address Book Program");
+        //    AddressBookMain bookLibrary = new AddressBookMain();
+        //    AddressBookMain.Menu();//going inside this class and method
+        //    MainMenu(string bookname)
+
+        //}
+
+    }
+
+}
+//to execute this
+//first we have to select the Option from Multiple addressBook in that 1 i
+//aftre that agin we have to open that addressbook to add the contact details
+//***************************UC11-sort albabatically******************************************//
+
+//******WELCOME TO ADDRESS BOOK******
+//1.Add_Contact
+//2.Display_Contact
+//3.Delet_Contact
+//4.Update_Contact
+//5.Serch_FromAllContact
+//6.Count_Contacts
+//7.Sort_Contacts
+//8.Exit
+//Enter Your Choice:
+//7
+//Alphabetically_Sorted_List
+//Luv
+//Sahu
+//******WELCOME TO ADDRESS BOOK******
+//1.Add_Contact
+//2.Display_Contact
+//3.Delet_Contact
+//4.Update_Contact
+//5.Serch_FromAllContact
+//6.Count_Contacts
+//7.Sort_Contacts
+//8
+//*******************************uc11***********************************//
+//Enter FirstName: Luv
+//Enter LastName: Sahu
+//Enter Address : hjhsdjklh lj
+//Enter City : asjd hj
+//Enter State : sjh jld
+//Enter zip : asjd jh
+//Enter Contact No: 515161
+//Enter Email: jhasbdj b
+//Details Added Successfully.
+
+//******WELCOME TO ADDRESS BOOK******
+//1.Add_Contact
+//2.Display_Contact
+//3.Delet_Contact
+//4.Update_Contact
+//5.Serch_FromAllContact
+//6.Count_Contacts
+//7.Sort_Contacts
+//8.Exit
+//Enter Your Choice:
+//7
+//Chooose option TO Sort Contacts by
+//1. FirstName
+//2. City
+//3. State
+//4. zip
+//2
+//Alphabetically_Sorted_List
+//----------------------------------------------------------------------
+//FirstName   LastName   Address, City, State, Zip, Contact, Email
+//----------------------------------------------------------------------
+//Shreegowri        koder           dsgh    Bng     kar    56788   wydhf    wjkdbc
+//gagana    koder           hvs     cta     mp     jwdvc   whdkvc           kwhd
+//******WELCOME TO ADDRESS BOOK******
+//1.Add_Contact
+//2.Display_Contact
+//3.Delet_Contact
+//4.Update_Contact
+//5.Serch_FromAllContact
+//6.Count_Contacts
+//7.Sort_Contacts
+//8.Exit
+//Enter Your Choice:
+
+//*************************uc13******************************//
+/*Enter FirstName: Luv
+Enter LastName: Sahu
+Enter Address : jdv
+Enter City : wjef
+Enter State : wjf
+Enter zip : ,wj
+Enter Contact No: dwb
+Enter Email: dbv
+Details Added Successfully.
+******WELCOME TO ADDRESS BOOK******
+1.Add_Contact
+2.Display_Contact
+3.Delet_Contact
+4.Update_Contact
+5.Serch_FromAllContact
+6.Count_Contacts
+7.Sort_Contacts
+8.Exit
+Enter Your Choice:
+8
+* *****WELCOME TO ADDRESS BOOK******
+1.Create_AddressBooks
+2.Open_AddressBooks
+3.Count_TotalContacts
+4.Serch_FromAllContact
+5.DeletAddressBook
+6.StoreContactsIn_TextFile
+7.ReadContactsFrom_TextFile
+8.Exit
+6
+You have 1 AddressBook.
+Shreegowri
+Enter Address_BookName : Shreegowri
+Contacts Stored in TextFile.
+******WELCOME TO ADDRESS BOOK******
+1.Create_AddressBooks
+2.Open_AddressBooks
+3.Count_TotalContacts
+4.Serch_FromAllContact
+5.DeletAddressBook
+6.StoreContactsIn_TextFile
+7.ReadContactsFrom_TextFile
+8.Exit
+7
+You have 1 AddressBook.
+Shreegowri
+Enter Address_BookName : Shreegowri
+FirstName       LastName         Address         City    State   Zip     Contact         Email
+Shreegowri      gfwhd   jdv     wjef    wjf     , wj     dwb     dbv
+******WELCOME TO ADDRESS BOOK******
+1.Create_AddressBooks
+2.Open_AddressBooks
+3.Count_TotalContacts
+4.Serch_FromAllContact
+5.DeletAddressBook
+6.StoreContactsIn_TextFile
+7.ReadContactsFrom_TextFile
+8.Exit*/
+
+
